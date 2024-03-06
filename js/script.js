473be415491fc9c51908ddfc4dacd88d532da549
Daniel Andŕes Vélez Vargas <daniel_andres.velez@uao.edu.co>
2024-03-06 15:30:58 -0500

    [ADD] Page Basic Structure and some JS

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-let test = "";
+// Declaración y asignación de variables para la obtención de los elementos HTML
+const menuBtn = document.querySelector(".menu-btn");
+const navbarMenu = document.querySelector(".navbar .menu");
+const menuBtnIcon = document.querySelector(".menu-btn i");
+
+// Funciones
+// Función para poner o quitar la clase active del menú y de su respectivo icono
+function toggleActiveMenu() {
+  navbarMenu.classList.toggle("active");
+  menuBtnIcon.classList.toggle("active");
+}
+
+// Uso del método de JavaScript "addEventListener" que permite ejecutar una función cuando ocurra el evento especificado en el elemento HTML obtenido
+
+// El primer argumento que se le entrega a la función es el evento y el segundo es la función que será ejecutada cuando ocurra dicho evento
+menuBtn.addEventListener("click", toggleActiveMenu);
